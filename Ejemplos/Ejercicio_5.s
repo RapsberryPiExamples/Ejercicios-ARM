251e7de73a89e67f064681ca04192e9b6d2d9bc8
RapsberryPiExamples <40403422+RapsberryPiExamples@users.noreply.github.com>
2018-06-19 16:08:05 -0600

    Add files via upload

---
+++ Ejemplos/Ejercicio_5.s
@@ -0,0 +1,44 @@
+/******************************************************************************
+  Ejercicio 5: Ejemplo de uso de la FPU ARM para calcular el resultado de la 
+  multiplicación de dos valores de punto flotante.
+******************************************************************************/
+ 
+.global main
+.func main
+   
+main:
+
+    LDR R0, =val1           @ load variable address
+    VLDR S0, [R0]           @ load the value into the VFP register
+    
+    LDR R0, =val2           @ load variable address
+    VLDR S1, [R0]           @ load the value into the VFP register
+    
+    VMUL.F32 S2, S0, S1     @ compute S2 = S0 * S1
+    
+    VCVT.F64.F32 D4, S2     @ covert the result to double precision for printing
+    VMOV R1, R2, D4         @ split the double VFP register into two ARM registers
+    BL  _printf_result      @ print the result
+    
+    B   _exit               @ branch to exit procedure with no return
+   
+_exit:  
+    MOV R7, #4              @ write syscall, 4
+    MOV R0, #1              @ output stream to monitor, 1
+    MOV R2, #21             @ print string length
+    LDR R1, =exit_str       @ string at label exit_str:
+    SWI 0                   @ execute syscall
+    MOV R7, #1              @ terminate syscall, 1
+    SWI 0                   @ execute syscall
+
+_printf_result:
+    PUSH {LR}               @ push LR to stack
+    LDR R0, =result_str     @ R0 contains formatted string address
+    BL printf               @ call printf
+    POP {PC}                @ pop LR from stack and return
+
+.data
+result_str:     .asciz      "Multiplication result = %f \n"
+exit_str:       .ascii      "Terminating program.\n"
+val1:           .float      3.14159
+val2:           .float      0.100
